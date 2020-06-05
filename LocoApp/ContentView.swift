341bda45f6128caebf8b8c9c299413bc9fa20e99
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-06-05 14:40:50 +0530

    Initial Commit

---
+++ LocoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocoApp
+//
+//  Created by Sakshi Jaiswal on 05/06/20.
+//  Copyright © 2020 Sakshi Jaiswal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
